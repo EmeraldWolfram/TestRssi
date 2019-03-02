b2d7f67eec567cc69e5d62904691736edb084876
Foong <ghin.yew.foong@intel.com>
2019-03-02 22:44:17 +0800

    Added Activity to for RSSI -> Distance testing

--- app/src/main/java/com/example/testrssi/MvpInterface/MvpRationAlgo.java
+++ app/src/main/java/com/example/testrssi/MvpInterface/MvpRationAlgo.java
@@ -1,4 +1,18 @@
 package com.example.testrssi.MvpInterface;
 
+import com.example.testrssi.model.Coordinate;
+
 public interface MvpRationAlgo {
+    interface MvpView {
+        void reportError(String msg);
+        void showCoordinate(Coordinate coor);
+        void showComputeTime(Long nanoTime);
+    }
+
+    interface MvpPresenterV {
+        void setupAccessPoints();
+        void onRefresh();
+    }
+
+    interface MvpModel {}
 }
--- app/src/main/java/com/example/testrssi/RationAlgoActivity.java
+++ app/src/main/java/com/example/testrssi/RationAlgoActivity.java
@@ -1,37 +1,67 @@
 package com.example.testrssi;
 
 import android.content.Context;
-import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.List;
+import com.example.testrssi.MvpInterface.MvpRationAlgo;
+import com.example.testrssi.model.Coordinate;
+import com.example.testrssi.presenter.RationAlgoPresenter;
 
-public class RationAlgoActivity extends AppCompatActivity {
 
-    private List<ScanResult> results;
+public class RationAlgoActivity extends AppCompatActivity implements MvpRationAlgo.MvpView {
+
+    private MvpRationAlgo.MvpPresenterV taskPresenter;
+    private TextView timeUsedView;
+    private TextView coorXView;
+    private TextView coorYView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ration_algo);
+        bindView();
+        initMvp();
     }
 
-    public void doCalculation(){
-        WifiManager wifi = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
-        if(wifi.startScan()) {
-            results = wifi.getScanResults();
-            //breakDownCoordinate();
-        } else {
-            Toast.makeText(this, "Start SCAN return NO", Toast.LENGTH_SHORT).show();
-        }
+    private void bindView(){
+        this.timeUsedView   = findViewById(R.id.timeUsed);
+        this.coorXView      = findViewById(R.id.coordinateX);
+        this.coorYView      = findViewById(R.id.coordinateY);
 
+    }
+
+    private void initMvp(){
+        WifiManager wifi    = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        this.taskPresenter  = new RationAlgoPresenter(this, wifi);
+        this.taskPresenter.setupAccessPoints();
     }
 
     public void onRefreshClicked(View view) {
-        // do REFRESH
+        this.taskPresenter.onRefresh();
+    }
+
+    @Override
+    public void reportError(String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void showComputeTime(Long nanoTime) {
+        String timeStr;
+        timeStr = nanoTime.toString() + "ns";
+        timeUsedView.setText(timeStr);
+    }
+
+    @Override
+    public void showCoordinate(Coordinate coor) {
+        String xStr = "X: " + coor.getCoorX().toString();
+        String yStr = "Y: " + coor.getCoorY().toString();
+        coorXView.setText(xStr);
+        coorYView.setText(yStr);
     }
 }
